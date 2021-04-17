fde3abb212c3f25234210028a85f3f86f04fe24c
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-04-17 17:16:00 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from covid import Covid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"""
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>COVID-19 Dashboard</title>
+</head>
+
+<body>
+    <h1>COVID-19 Dashboard (Worldwide)</h1>
+    <hr/>
+    <h3>Confirmed: {Covid().get_total_confirmed_cases()}</h3>
+    <h3>Deaths: {Covid().get_total_deaths()}</h3>
+    <h3>Recovered: {Covid().get_total_recovered()}</h3>
+</body>
+
+</html>
+"""
+
+app.run()
